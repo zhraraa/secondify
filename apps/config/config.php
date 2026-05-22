924429b43590630d85816cc42146a8d6bfa336d4
Alyssa <lisxph07@gmail.com>
2026-05-22 19:34:12 +0700

    fix dikit setting

--- apps/config/config.php
+++ apps/config/config.php
@@ -1,10 +1,13 @@
 <?php
 $lifetime = 60;
-session_set_cookie_params($lifetime);
-ini_set('session.gc_maxlifetime', $lifetime);
-if (session_status() == PHP_SESSION_NONE) {
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params($lifetime);
+    ini_set('session.gc_maxlifetime', $lifetime);
     session_start();
 }
 
-define('SECONDIFY', 'http://localhost/secondify');
+if (!defined('SECONDIFY')) {
+    define('SECONDIFY', 'http://localhost/secondify');
+}
 ?>
